f05530c10eb1d0a5d2bef732879d15a45efc08ca
Nate Wessels and Tien Le <nate@>
2019-01-22 11:09:05 -0800

    add looping

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,16 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    var variables = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+
+    variables.forEach(function(variable){
+      var userInput = $("input#" + variable).val();
+      $("." + variable).text(userInput);
+    });
+
+
+
+
 
     $("#story").show();
 
